3dd299b2e455ce172b51bff811a0354703adb45a
cn22tm <cn222tm@student.lnu.se>
2020-09-28 14:16:07 +0200

    Generation of memberID working

--- src/Authentication.java
+++ src/Authentication.java
@@ -6,9 +6,12 @@ public class Authentication {
         Scanner scan = new Scanner(System.in);
         System.out.print("Welcome to BoatClub!\n\n\t1. Login\n\t2. Register\n\t3. Exit\n\nSelect option above: ");
         String input = scan.nextLine();
-        if (input.equals("1")) login();
-        else if (input.equals("2")) register();
-        else if (input.equals("3")) return false;
+        if (input.equals("1"))
+            login();
+        else if (input.equals("2"))
+            register();
+        else if (input.equals("3"))
+            return false;
         else
             System.out.println("\n\nInvalid input try again!\n\n");
             askLoginOrRegistration();
@@ -19,12 +22,32 @@ public class Authentication {
 
     }
 
-    public static void invalidLogin() {
+    private static void invalidLogin() {
 
     }
 
-    public static void register () {
+    private static void register () {
+        int personalNum;
+        String fullName;
+        String password;
+        String memberId;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\n\nFull name: ");
+        fullName = scan.nextLine();
+        System.out.print("\n\nPassword: ");
+        password = scan.nextLine();
+        System.out.print("\n\n(Has to be only numbers) Personal number: ");
+        personalNum = scan.nextInt();
+        memberId = generateMemberID(fullName);
+    }
 
+    private static String generateMemberID(String fullName) {
+        String[] name = fullName.split(" ");
+        String firstName = String.valueOf(name[0].charAt(0));
+        String lastName = String.valueOf(name[1].charAt(0));
+        int randomNum = (int)(Math.random()*9000) + 1000;
+        String memberId = firstName + lastName + randomNum;
+        return memberId;
     }
 
 
