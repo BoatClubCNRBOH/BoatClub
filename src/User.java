c9ec049095d6367fce82623363efb50ecd1e51b3
cn22tm <cn222tm@student.lnu.se>
2020-09-28 12:24:41 +0200

    methods for user added

--- src/User.java
+++ src/User.java
@@ -1,4 +1,25 @@
 public class User {
     private String memberID;
     private int permissionLevel;
+
+    public User(String memID, int permLvl){
+        memberID = memID;
+        permissionLevel = permLvl;
+    }
+
+    public void deleteMember(int permissionLevel) {
+
+    }
+
+    public void changeInformation() {
+
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public void listMembers(int permissionLevel) {
+
+    }
 }
